569277dbf3b32cdffd70c220d342506db953a5f9
fzy-dot <1661875259@qq.com>
2023-03-13 16:54:39 +0800

    1

---
+++ pages/user-center/siteinfo.js
@@ -0,0 +1,33 @@
+// // #ifdef MP
+// module.exports = {
+//   acid: -1,
+//   version: "1.0.0",
+//   // apiroot: "api/mobil/web/index.php?_mall_id=1",
+//   id: "1",
+//   platform: "mobile",
+//   // 'siteroot': 'https://meifu.vip/app/index.php',
+//   // 'apiroot': 'https://meifu.vip/web/index.php?_mall_id=1',
+//   siteroot: "https://dev-mall.kaytune.com/mobil/app/index.php",
+//   // apiroot: "https://dev-mall.kaytune.com/mobil/web/index.php?_mall_id=1",
+//   apiroot: "https://portal-mobil.kaytune.com.cn/web/index.php?_mall_id=1", //正式环境
+  
+// };
+// // #endif
+//h5
+// #ifdef H5
+let siteInfo = {
+    // 'acid': -1,
+    // 'version': '1.0.0',
+    // 'siteroot': 'https://meifu.vip/app/index.php',
+    // 'apiroot': 'https://meifu.vip/web/index.php?_mall_id=1',
+    // 'siteroot': 'api/mobil/app/index.php',
+    // 'apiroot': 'api/mobil/web/index.php?_mall_id=1',
+    // 'id': '1',
+    // 'platform': 'wxapp' //'mobile' 'wechat'  'wxapp'
+    'acid': -1,
+    'version': '1.0.0',
+    'apiroot': 'api/web/index.php?_mall_id=1',
+    'id': '1',
+    'platform': 'mobile',
+};
+// #endif
---
+++ version.js
@@ -0,0 +1,8 @@
+// 小程序
+// // #ifdef MP
+// module.exports = '4.8.9';
+// // #endif
+//  h5
+// #ifdef H5
+let version = "4.8.12";
+// #endif
